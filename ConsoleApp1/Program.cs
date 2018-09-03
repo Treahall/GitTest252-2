0ed8293b25f14a604493689ae3ed9786e5662264
Trever Hall <treahall@ut.utm.edu>
2018-09-03 16:34:49 -0500

    Guess a random number "game" added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,37 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I am thinking of a number between 1 and 100");
+            
+            Random random = new Random(100);
+
+            int Mynum = random.Next(0, 100);
+            int guess = -1;
+            int numTries = 0;
+
+            do
+            {
+                Console.WriteLine("==========================================");
+                Console.Write("Guess the number: ");
+                string userInput = Console.ReadLine();
+
+                guess = Convert.ToInt32(userInput);
+
+                if (guess == Mynum)
+                    Console.WriteLine("You guessed it right!");
+                else if (guess > Mynum)
+                    Console.WriteLine("You were too high :(");
+                else if (guess < Mynum)
+                    Console.WriteLine("You were too low :(");
+
+                numTries++;
+            } while (Mynum != guess);
+
+            Console.WriteLine("==========================================");
+            Console.WriteLine("It took you '{0}' tries", numTries);
+            Console.WriteLine("Press any key to exit");
+
+            Console.ReadKey();
 
         }
     }
